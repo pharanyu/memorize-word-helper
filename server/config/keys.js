8b9773f3af066b3747f3ee44175ff1a3f30c7ef5
pharanyu <pharanyu.nunui@gmail.com>
2019-10-08 23:53:09 +0700

    update

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,3 +1,9 @@
-module.exports = {
-  MongoURL: 'mongodb+srv://pharanyu:13699631@shopper-db-sgbhs.mongodb.net/WordsCollection?retryWrites=true&w=majority'
+if (process.env.NODE_ENV === "production") {
+  module.exports = {
+    MongoURL: process.env.MONGO_URI
+  };
+} else {
+  module.exports = {
+    MongoURL: "mongodb+srv://pharanyu:13699631@shopper-db-sgbhs.mongodb.net/WordsCollection?retryWrites=true&w=majority"
+  };
 }
