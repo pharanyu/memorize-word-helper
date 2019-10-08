f262d4aaca6923b2eeb28716bbb50edbaf765d18
pharanyu <pharanyu.nunui@gmail.com>
2019-10-09 00:09:46 +0700

    update

--- src/app/services/url.service.ts
+++ src/app/services/url.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { isDevMode } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -6,21 +8,33 @@ import { Injectable } from '@angular/core';
 export class UrlService {
   private urlConnectServer = 'http://localhost:3001/api/words';
 
-  constructor() {}
+  constructor() { }
+
+  CheckModeUrl(): void {
+    if (isDevMode()) {
+      this.urlConnectServer = 'http://localhost:3001/api/words';
+    } else {
+      this.urlConnectServer = `https://memorize-word-helper.herokuapp.com/api/words`;
+    }
+  }
 
   reqGroupUrl(): string {
+    this.CheckModeUrl();
     return this.urlConnectServer;
   }
 
   reqWordsOfGroupUrl(reqGroup: string): string {
+    this.CheckModeUrl();
     return `${this.urlConnectServer}/${reqGroup}`;
   }
 
   reqRenameGroupUrl(oldName: string, newName: string): string {
+    this.CheckModeUrl();
     return `${this.urlConnectServer}/renamegroup/${oldName}/${newName}`;
   }
 
   reqDeleteWordsUrl(): string {
+    this.CheckModeUrl();
     return `${this.urlConnectServer}/delete`;
   }
 }
