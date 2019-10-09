25a92b40eaa841a450693b97e32a2978aab626ff
pharanyu <pharanyu.nunui@gmail.com>
2019-10-09 08:51:45 +0700

    Update url.service.ts

--- src/app/services/url.service.ts
+++ src/app/services/url.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class UrlService {
-  private urlConnectServer = 'http://localhost:3001/api/words';
+  private urlConnectServer = '/api/words';
 
   constructor() {}
 
