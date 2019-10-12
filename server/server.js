bb2ee3a7370c2f203df22b104ac8e45104bc818e
pharanyu <pharanyu.nunui@gmail.com>
2019-10-12 23:06:06 +0700

    update server

--- server/server.js
+++ server/server.js
@@ -25,5 +25,21 @@ app.use('/', express.static(path.join(__dirname, '../dist/memorize-word-helper')
 // ROUTES
 app.use('/api/words', require('./routes/wordsRoute'));
 
+// If an incoming request uses
+// a protocol other than HTTPS,
+// redirect that request to the
+// same url but with HTTPS
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+app.use(forceSSL());
+
 const server = http.createServer(app);
 server.listen(PORT, () => console.log('Server running...'));
