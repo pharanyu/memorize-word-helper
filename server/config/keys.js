60c05febee3cee60104540d06783df306e3ff951
pharanyu <pharanyu.nunui@gmail.com>
2019-10-13 11:10:54 +0700

    update server

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,9 +1,9 @@
-if (process.env.NODE_ENV === "production") {
+/*if (process.env.NODE_ENV === "production") {
   module.exports = {
     MongoURL: process.env.MONGO_URI
   };
-} else {
+} else {*/
   module.exports = {
     MongoURL: "mongodb+srv://pharanyu:13699631@shopper-db-sgbhs.mongodb.net/WordsCollection?retryWrites=true&w=majority"
   };
-}
+/*}*/
