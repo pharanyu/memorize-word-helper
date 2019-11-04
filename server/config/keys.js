929f88c82cfd4e75d990b9a09272da3439b75b5c
pharanyu <pharanyu.nunui@gmail.com>
2019-11-04 22:25:34 +0700

    update

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  JWT_SECRET: 'dev#secret' /*process.env.JWT_SECRET*/,
-  JWT_EXP: '2m' /*process.env.JWT_EXP*/,
+  JWT_SECRET: /*'dev#secret'*/ process.env.JWT_SECRET,
+  JWT_EXP: /*'2m'*/ process.env.JWT_EXP,
   MongoURL: process.env.MONGO_URI
 };
