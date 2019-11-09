2788d0f31be4e78f38fcd272b1fa5c614ae9dab2
pharanyu <pharanyu.nunui@gmail.com>
2019-11-09 22:22:09 +0700

    update

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  JWT_SECRET: 'dev#secret' /*process.env.JWT_SECRET*/,
-  JWT_EXP: '2m' /*process.env.JWT_EXP*/,
-  MongoURL: 'mongodb://localhost:27017/TestMemoWord'/*process.env.MONGO_URI*/
+  JWT_SECRET: /*'dev#secret'*/ process.env.JWT_SECRET,
+  JWT_EXP: /*'2m'*/ process.env.JWT_EXP,
+  MongoURL: process.env.MONGO_URI
 };
